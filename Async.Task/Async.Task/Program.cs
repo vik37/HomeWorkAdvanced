b5b848df32054e539763cd55ad26953f933aa101
vik37 <59177404+vik37@users.noreply.github.com>
2020-07-08 15:14:35 +0200

    Little Task

---
+++ Async.Task/Async.Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                var index = i;
+
+                var result = Task<string>.Run(() =>
+                {
+                    Thread.Sleep(1000);
+                    return $"Task number {index}";
+                });
+
+                Console.WriteLine(result.Result);
+                if (index == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("HAPPY NEW YEAR!!!!");
+                    Console.ResetColor();
+                }
+            }
+        
+            Console.ReadLine();
+        }
+    }
+}
